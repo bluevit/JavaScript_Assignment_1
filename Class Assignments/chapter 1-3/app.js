044e58bd8dcfab40ef5288c2765a616254f5435f
Muhammad Raza <118042487+bluevit@users.noreply.github.com>
2024-09-08 12:48:08 +0500

    Add files via upload

---
+++ Class Assignments/chapter 1-3/app.js
@@ -0,0 +1,63 @@
+//Chapter 1 (alerts)
+//Question 1
+var firstName = 'Muhammad';
+var lastName = 'Raza';
+var email = 'mrazaiqbal2002@gmail.com';
+var ph_num = '03XX-XXXXXXX';
+var pass = '123456789';
+alert(firstName);
+alert(lastName);
+alert(email);
+alert(ph_num);
+alert(pass);
+
+//Question 2
+alert("Your're learning JavaScript!");
+
+//Question 3
+alert('Alert pops alert');
+
+
+
+//Chapter 2 (Variables for Strings)
+//Question 1
+var varName;
+
+//Question 2
+var education;
+education = "Bachelors in Science";
+
+//Question 3
+var teamName = 'Blue Blaze';
+alert(teamName);
+
+//Question 4
+var bestMan = "Charlie";
+bestMan = "Raza";
+
+
+
+//Chapter 3 (Variables for Numbers)
+//Question 1
+var caseQty;
+caseQty = 144;
+
+//Question 2
+var num = 9;
+alert('Variable num performing sum = '+ (num+1));
+
+//Question 3
+var sum;
+sum = 10+10;
+alert('sum = '+sum);
+
+//Question 4
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert('Order Total = '+orderTotal);
+
+//Question 5
+var operation = 99;
+operation = operation+1;
+alert(operation);
---
+++ Class Assignments/javaScript.js
@@ -0,0 +1,114 @@
+//console.log('Hello') //js bultin function
+// document.write('Hello')
+// alert('ASSALAM O ALAIKUM NAEEM BHAI');  
+// var name = 'Raza';
+// console.log('My name is',name);
+// console.log(`Halo, caffee und brod bitte, danke \nich bin ${name} \nWie geht es dir?`)
+
+//chapter 1
+//QUESTION 1
+alert("'Greetings User'");
+
+//QUESTION 2
+alert('Please enter a valid password.');
+
+//QUESTION 3
+alert('Welcome to JS land...\nHappy Coding!');
+
+//QUESTION 4
+alert('Welcome to JS land...');
+alert('Happy Coding!');
+
+//QUESTION 5
+alert("Hello, I can run JS through my web browser's console.");
+
+
+
+
+//chapter 2
+//QUESTION 1
+var username;
+
+//QUESTION 2
+var myname = 'Muhammad Raza';
+
+//QUESTION 3
+var message;
+message = 'Hello, World';
+alert(message);
+
+//QUESTION 4
+var name = 'John Doe';
+var age = 15;
+var qualification = 'Certified Mobile Application Developer';
+alert(name);
+alert(`${age} years old`);
+alert(qualification);
+
+//QUESTION 5
+var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+alert(pizza);
+
+//QUESTION 6
+var eamil = 'example@example.com';
+alert('My email address is '+eamil);
+
+//QUESTION 7
+var book = 'A Smarter Way to Learn JavaScript';
+alert(`I am trying to learn from a book ${book}`);
+
+//QUESTION 8
+document.write('Yah! I can write HTML content through JavaScript');
+
+//QUESTION 9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+
+//chapter 3
+//QUESTION 1
+var age = 21;
+alert('I am '+age+' years old.');
+
+//QUESTION 2
+var vists = 14;
+alert('You have visited this site '+vists+' times.');
+
+//QUESTION 3
+var year = 2002;
+document.write(`My birth year is ${year} 
+    \nData type of my declared variable is ${typeof year}.`);
+
+//QUESTION 4
+var customer = 'John Doe';
+var quantity = 5;
+var visitor = `${customer} ordered ${quantity} T-shirt(s) on XYZ Clothing store.<br>`;
+document.write(visitor);
+
+
+//chapter 4
+//QUESTION 1
+var var1 , var2, var3;
+
+//QUESTION 2
+//legal
+var userName;
+var _age;
+var $price;
+var first_name;
+var total5;
+
+//illegal
+// 2ndPlace
+// let
+// user-name
+// @data
+// var!
+
+//QUESTION 3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, dollar sign ($), or underscore (_). For example $name, _name, or name</p>");
+document.write("<p>Variable names are case sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
